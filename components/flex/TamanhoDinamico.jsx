88cabcade73deff720760028038c486889af0c32
pedrohpol <pedroholiveira@alu.ufc.br>
2022-09-24 18:21:16 -0300

    atividade 02

---
+++ components/flex/TamanhoDinamico.jsx
@@ -0,0 +1,26 @@
+import {View} from "react-native";
+
+const TamanhoDinamico = () => {
+    return (
+        <View style = {{
+                flex: 1, 
+                flexDirection: "column",
+                flexWrap: "wrap",
+            }}>
+            <View style = {{width: 100, height: 100, backgroundColor: "steelblue"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "yellow"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "green"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "steelblue"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "yellow"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "green"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "steelblue"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "yellow"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "green"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "steelblue"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "yellow"}}></View>
+            <View style = {{width: 100, height: 100, backgroundColor: "green"}}></View>
+        </View>
+    )
+}
+
+export default TamanhoDinamico;
